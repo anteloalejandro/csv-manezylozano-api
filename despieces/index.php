02f90addb4ee9414f76631ee98ec11c0b3bf7c58
Alejandro Antelo <alfasant@protonmail.com>
2023-09-07 13:52:36 +0200

    añadir funciona, falta actualizar

--- despieces/index.php
+++ despieces/index.php
@@ -12,13 +12,189 @@ require_once(__DIR__ . '/../helpers/csv.php');
 
 $testing = true; // Cambiar a false en el servidor
 
-if ($testing || !isAllowed()) {
+if (!($testing || isAllowed())) {
   CsvImportResponse::failure([], 'No se ha podido iniciar sesión');
-} else if (!isset($_FILES['file'])) {
+} else if (!isset($_FILES['despiece-csv'])) {
   CsvImportResponse::failure([], 'Archivo no subido correctamente');
-} else if (stripos($_FILES['file']['name'], '.csv') === false) {
+} else if (stripos($_FILES['despiece-csv']['name'], '.csv') === false) {
   CsvImportResponse::failure([], 'El archivo debe tener formato CSV');
 }
 
-$rows = readCSV($_FILES['file']['tmp_name']);
+$rows = readCSV($_FILES['despiece-csv']['tmp_name']);
+
+usort($rows, function ($a, $b) {
+  $a_pos = $a[2];
+  $b_pos = $b[2];
+
+  return (int)$a_pos - (int)$b_pos;
+});
 $warnings = [];
+
+delete_old_assemblies();
+for ($i = 1; $i < count($rows); $i++) {
+  $row = $rows[$i];
+  if (!is_array($row)) {
+    array_push($warnings, new CsvWarning(
+      'pieza',
+      'none',
+      "Error de formato en la fila " . $i + 1
+    ));
+    continue;
+  }
+  try {
+    add_assembly($row, $i, $warnings);
+  } catch (Throwable $e) {
+    CsvImportResponse::failure($warnings, $e->getMessage());
+  }
+}
+
+CsvImportResponse::success($warnings);
+
+function add_assembly(array $row, $idx, &$warning_arr)
+{
+  [$category_name, $assembly_name, $part_position, $part_ref, $part_qty] = $row;
+
+  $part_id = wc_get_product_id_by_sku($part_ref);
+  if ($part_id <= 0) {
+    array_push($warning_arr, new CsvWarning('part', $part_ref, "Pieza $part_ref no encontrada"));
+    return;
+  }
+  $assemblies = get_posts([
+    'name' => sanitize_title($assembly_name),
+    'post_type' => 'product'
+  ]);
+
+  $assembly_id = null;
+  if (count($assemblies) > 0) {
+    $assembly_id = is_int($assemblies[0])
+      ? $assemblies[0]
+      : $assemblies[0]->ID;
+  } else {
+    $assembly_id = wp_insert_post([
+      'post_author' => 1,
+      'post_content' => '',
+      'post_status' => "publish",
+      'post_title' => $assembly_name,
+      'post_name' => sanitize_title($assembly_name),
+      'post_parent' => '',
+      'post_type' => "product"
+    ]);
+  }
+
+  create_category_if_not_exists($category_name);
+  wp_set_object_terms($assembly_id, $category_name, 'product_cat');
+  convert_to_bundle($assembly_id);
+  add_part_to_bundle($assembly_id, $part_id, $part_ref, $part_qty, $idx);
+}
+
+function create_category_if_not_exists($category_name)
+{
+  $categories = get_terms(['taxonomy' => 'product_cat']);
+  foreach ($categories as $cat) {
+    if ($cat->name == $cat || $cat->slug == sanitize_title($category_name)) {
+      return;
+    }
+  }
+
+  wp_insert_term(
+    $category_name,
+    'product_cat',
+    [
+      'description' => '',
+      'slug' => sanitize_title($category_name)
+    ]
+  );
+}
+
+function convert_to_bundle($assembly_id)
+{
+  wp_set_object_terms($assembly_id, 'woosb', 'product_type');
+  update_post_meta($assembly_id, 'woosb_optional_products', 'on'); // Editar nª de piezas
+
+  update_post_meta($assembly_id, 'total_sales', '0');
+  update_post_meta($assembly_id, '_tax_status', 'taxable');
+  update_post_meta($assembly_id, '_tax_class', '');
+  update_post_meta($assembly_id, '_manage_stock', 'no');
+  update_post_meta($assembly_id, '_backorders', 'no');
+  update_post_meta($assembly_id, '_sold_individually', 'no');
+  update_post_meta($assembly_id, '_virtual', 'no');
+  update_post_meta($assembly_id, '_downloadable', 'no');
+  update_post_meta($assembly_id, '_download_limit', '-1');
+  update_post_meta($assembly_id, '_download_expiry', '-1');
+  update_post_meta($assembly_id, '_stock', 'NULL');
+  update_post_meta($assembly_id, '_stock_status', 'instock');
+  update_post_meta($assembly_id, '_wc_average_rating', '0');
+  update_post_meta($assembly_id, '_wc_average_count', '0');
+  update_post_meta($assembly_id, 'woosb_disable_auto_price_off', 'off');
+  update_post_meta($assembly_id, 'woosb_discount', '');
+  update_post_meta($assembly_id, 'woosb_discount_amount', '');
+  update_post_meta($assembly_id, 'woosb_shipping_fee', 'whole');
+  update_post_meta($assembly_id, 'woosb_manage_stock', 'off');
+  update_post_meta($assembly_id, 'woosb_limit_each_min', '');
+  update_post_meta($assembly_id, 'woosb_limit_each_min_default', 'off');
+  update_post_meta($assembly_id, 'woosb_limit_whole_min', '');
+  update_post_meta($assembly_id, 'woosb_limit_whole_max', '');
+  update_post_meta($assembly_id, 'woosb_total_limits', 'off');
+  update_post_meta($assembly_id, 'woosb_total_limits_min', '');
+  update_post_meta($assembly_id, 'woosb_total_limits_max', '');
+  update_post_meta($assembly_id, 'woosb_layout', 'unset');
+  update_post_meta($assembly_id, '_edit_lock', NULL); // https://wordpress.stackexchange.com/questions/135480/why-are-simple-updates-to-wp-postmetas-edit-lock-so-slow
+  update_post_meta($assembly_id, '_edit_last', '1');
+
+}
+
+function add_part_to_bundle($idDespiece, $product_id, $ref, $quantity = '1', $idx = 0)
+{
+  $despiece_bundle = get_post_meta($idDespiece, 'woosb_ids', true);
+  if (is_serialized($despiece_bundle)) {
+    unserialize($despiece_bundle);
+  }
+  if ($despiece_bundle == "") {
+    $despiece_bundle = [];
+  }
+
+
+  $bundle_id = '';
+  if ($idx <= 0) {
+    $bundle_id = bin2hex(random_bytes(7));
+    $bundle_id = substr_replace($bundle_id, '', 0, 1); // truncar 1 para tener 13
+  } else {
+    $bundle_id = sprintf("%'.013d", $idx);
+  }
+
+  $bundle_item = [
+    $bundle_id => [
+      'id' => "$product_id",
+      'sku' => $ref,
+      'qty' => "$quantity"
+    ]
+  ];
+  $replaced = false;
+  foreach ($despiece_bundle as &$despiece) {
+    if ($despiece['sku'] == reset($bundle_item)['sku']) {
+      $despiece = reset($bundle_item);
+      $replaced = true;
+      break;
+    }
+  }
+  if (!$replaced) {
+    $despiece_bundle += $bundle_item;
+  }
+
+  update_post_meta($idDespiece, 'woosb_ids', $despiece_bundle);
+}
+
+function delete_old_assemblies()
+{
+  /* TO-DO
+      Borrar tras acabar de insertar
+  */
+  $existing_assemblies = get_posts([
+    'post_type' => 'product',
+    'meta_key' => 'woosb_ids'
+  ]);
+  foreach ($existing_assemblies as $a) {
+    $id = is_int($a) ? $a : $a->ID;
+    wp_delete_post($id, true);
+  }
+}
--- piezas/index.php
+++ piezas/index.php
@@ -10,9 +10,9 @@ require_once(__DIR__ . '/../helpers/string.php');
 require_once(__DIR__ . '/../helpers/login.php');
 require_once(__DIR__ . '/../helpers/csv.php');
 
-$testing = false; // Cambiar a false en el servidor
+$testing = true; // Cambiar a false en el servidor
 
-if ($testing || !isAllowed()) {
+if (!($testing || isAllowed())) {
   CsvImportResponse::failure([], 'No se ha podido iniciar sesión');
 } else if (!isset($_FILES['file'])) {
   CsvImportResponse::failure([], 'Archivo no subido correctamente');
@@ -37,7 +37,7 @@ for ($i = 1; $i < count($rows); $i++) {
     continue;
   }
   try {
-    add_part($row, $i, $idDespiece, $warnings);
+    add_part($row, $i, $warnings);
   } catch (Throwable $e) {
     CsvImportResponse::failure($warnings, $e->getMessage());
   }
@@ -46,7 +46,7 @@ for ($i = 1; $i < count($rows); $i++) {
 CsvImportResponse::success($warnings);
 
 
-function add_part(array $row, int $position, $idDespiece, &$warning_arr)
+function add_part(array $row, int $position, &$warning_arr)
 {
   /* TO-DO
       Visibilidad
@@ -54,12 +54,11 @@ function add_part(array $row, int $position, $idDespiece, &$warning_arr)
   [$ref, $name, $price, $estanteria] = $row;
   $price = str_replace(',', '.', $price);
 
-  $post_id = wc_get_product_id_by_sku($ref); // No funciona
+  $post_id = wc_get_product_id_by_sku($ref);
   if ($post_id) {
     $post = get_post($post_id);
     $post->post_title = $name;
     $post->post_name = sanitize_title($name);
-    $post->menu_order = $position;
     $post_id = wp_update_post($post);
   } else {
     $post_id = wp_insert_post([
@@ -69,34 +68,13 @@ function add_part(array $row, int $position, $idDespiece, &$warning_arr)
       'post_title' => $name,
       'post_name' => sanitize_title($name),
       'post_parent' => '',
-      'post_type' => "product",
-      'menu_order' => $position
+      'post_type' => "product"
     ]);
   }
 
   $product = wc_get_product($post_id);
   $product->set_sku($ref);
   $product->save();
-
-  try {
-    add_part_to_bundle($idDespiece, $post_id, $ref, 1);
-  } catch (Exception $e) {
-    array_push($warning_arr, new CsvWarning(
-      'pieza',
-      $ref,
-      'Error al añadir al conjunto: ' . $e->getMessage()
-    ));
-  }
-
-  try {
-    set_category($idDespiece, $post_id);
-  } catch (Exception $e) {
-    array_push($warning_arr, new CsvWarning(
-      'pieza',
-      $ref,
-      'Error al establecer la categoría: ' . $e->getMessage()
-    ));
-  }
 
   try {
     set_attrs($post_id, $ref, $price);
@@ -120,67 +98,16 @@ function add_part(array $row, int $position, $idDespiece, &$warning_arr)
     ));
   }
 
-}
-
-function set_category($idDespiece, $post_id)
-{
-  $categorias_despiece = get_the_terms($idDespiece, 'product_cat');
-  $nombre_categoria = 'Piezas ' . $categorias_despiece[0]->name;
-  $all_categories = get_categories();
-  $exists = false;
-  foreach ($all_categories as $category) {
-    if ($category->slug == $categorias_despiece[0]->slug) {
-      $exists = true;
-      break;
-    }
-  }
-
-  if (!$exists) {
-    wp_insert_term(
-      $nombre_categoria,
-      'product_cat',
-      [
-        'description' => '',
-        'slug' => 'piezas-' . $categorias_despiece[0]->slug,
-        'parent' => $categorias_despiece[0]->term_id
-      ]
-    );
-  }
-
-  wp_set_object_terms($post_id, $nombre_categoria, 'product_cat');
-}
-
-function add_part_to_bundle($idDespiece, $product_id, $ref, $quantity = '1')
-{
-  $despiece_bundle = get_post_meta($idDespiece, 'woosb_ids', true);
-  if (is_serialized($despiece_bundle)) {
-    unserialize($despiece_bundle);
-  }
-  if ($despiece_bundle == "") {
-    $despiece_bundle = [];
-  }
-  $bundle_id = bin2hex(random_bytes(7));
-  $bundle_id = substr_replace($bundle_id, '', 0, 1); // truncar 1 para tener 13
-  $bundle_item = [
-    $bundle_id => [
-      'id' => "$product_id",
-      'sku' => $ref,
-      'qty' => "$quantity"
+  wp_insert_term(
+    'Piezas',
+    'product_cat',
+    [
+      'description' => '',
+      'slug' => 'piezas',
     ]
-  ];
-  $replaced = false;
-  foreach ($despiece_bundle as &$despiece) {
-    if ($despiece['sku'] == reset($bundle_item)['sku']) {
-      $despiece = reset($bundle_item);
-      $replaced = true;
-      break;
-    }
-  }
-  if (!$replaced) {
-    $despiece_bundle += $bundle_item;
-  }
+  );
+  wp_set_object_terms($post_id, 'Piezas', 'product_cat');
 
-  update_post_meta($idDespiece, 'woosb_ids', $despiece_bundle );
 }
 
 function set_attrs($post_id, $ref, $price)
